b175934b7ba06f2915c06bd15e0391dd58e67b48
Gabriela Froener <gabriela.froener@gmail.com>
2019-04-08 16:43:31 -0300

    fixing identation on actions

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,10 @@
+import { saveQuestionAnswer } from '../utils/api'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
 
 export function receiveQuestions(questions) {
-  return {
-    type: RECEIVE_QUESTIONS,
-    questions,
-  }
+	return {
+		type: RECEIVE_QUESTIONS,
+		questions,
+	}
 }
